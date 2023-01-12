922ead79c2603a112c85fdab9c0906b367cbb70d
DanikMonday <daacostaf@unal.edu.co>
2023-01-12 18:16:03 -0500

    first

---
+++ src/Components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { AiFillHome } from "react-icons/ai";
+import { AiFillFolder} from "react-icons/ai";
+import { GoPerson } from "react-icons/go";
+import { RiBook2Fill } from "react-icons/ri";
+
+export const Header = () => {
+  return (
+    <div className='head'>
+        <div className='logo-cont'>
+            <img className="logo" src={require("../Images/logo6.png")} alt="logo"></img>
+        </div>
+        <div className='nav'>
+            <div className='nav-up'>
+                <p><AiFillHome/> Home </p>
+                <p><GoPerson/> About </p>
+                <p><RiBook2Fill/> XP </p>
+            </div>
+            <div className='nav-down'>
+                <p><AiFillFolder/>Works</p>
+                <p><AiFillHome/>Contact</p>
+            </div>
+        </div>
+    </div>
+  )
+}
---
+++ src/Components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+export const Home = () => {
+  return (
+    <div className='home'>
+       <div className='profile-img'></div>
+       <div className='name'>DANIELA ACOSTA F.</div>
+       <div className='networks'>
+            
+       </div>
+       <button className='btn'></button>
+    </div>
+  )
+}
