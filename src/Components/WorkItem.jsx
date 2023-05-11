9d26d2649e688caf2b379b37f92006b97d531f62
DanikMonday <daacostaf@unal.edu.co>
2023-05-11 12:10:31 -0500

    works3

--- src/Components/WorkItem.jsx
+++ src/Components/WorkItem.jsx
@@ -5,7 +5,8 @@ export const WorkItem = (props) => {
     
         
         <div className='a-work'  style={{ 
-          backgroundImage: `url("${props.fondo}")` 
+          backgroundImage: `url("${props.fondo}")` ,
+          backgroundSize: `cover` 
         }}>
         <a href={props.deploy} target="blank" ><h2>{props.titulo}</h2></a>
         </div>
